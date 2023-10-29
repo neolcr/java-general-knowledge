2bda222cb629e6c25b244019c8e0e8cf93886a62
enol <enolcacheroramirez@gmail.com>
2023-10-29 20:22:38 +0100

    Security reactive

--- project-a/security/src/main/java/com/neol/security/config/SecurityConfig.java
+++ project-a/security/src/main/java/com/neol/security/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.neol.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -25,6 +27,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http){
+        return http
+                .httpBasic()
+                .and()
+                    .authorizeExchange()
+                        .pathMatchers("/demo/**").authenticated()
+                        .anyExchange().permitAll()
+                .and()
+                    .build();
     }
 }
--- project-a/security/src/main/java/com/neol/security/controllers/DemoController.java
+++ project-a/security/src/main/java/com/neol/security/controllers/DemoController.java
@@ -1,20 +1,27 @@
 package com.neol.security.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello";
+    @GetMapping(value = "/demo", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<String> demo(){
+        Mono<SecurityContext> context = ReactiveSecurityContextHolder.getContext();
+        context.map(SecurityContext::getAuthentication)
+                .log()
+                .subscribe( authentication -> System.out.println(authentication.getAuthorities()));
+        return Mono.just("Hello from demo");
     }
 
-    @PostMapping("/hello")
-    public String phello(){
-        return "Post hello";
+    @GetMapping(value = "/hello", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Mono<String> hello(){
+        return Mono.just("Hello from hello");
     }
+
 }
